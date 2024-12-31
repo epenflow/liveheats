c57900943abf7f911ab5ae5d1a75995dd19a78ae
epenflow <epenflow@gmail.com>
2024-12-31 20:17:40 +0800

    feat(config): Create GSAP configuration files

---
+++ src/config/gsap.ts
@@ -0,0 +1,7 @@
+'use client';
+import { useGSAP } from '@gsap/react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+export { gsap, ScrollTrigger, useGSAP };
+export default gsap;
