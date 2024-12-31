e73213695e68acb682dfa6c5e41a5b51ec140424
epenflow <epenflow@gmail.com>
2024-12-31 21:11:45 +0800

    fix(hoc): Disable eslint import/no-anonymous default and enhance null safety

--- src/components/base/heat/hoc.tsx
+++ src/components/base/heat/hoc.tsx
@@ -1,16 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
 'use client';
 import gsap, { useGSAP } from '@/config/gsap';
 import React from 'react';
 export interface Props {
 	scope: React.RefObject<HTMLDivElement | null>;
 }
-export default function <T extends {}>(Component: React.ComponentType<T & Props>) {
+export default function <T extends object>(Component: React.ComponentType<T & Props>) {
 	return function HOC(props: T) {
 		const scope = React.useRef<HTMLDivElement | null>(null);
 
 		useGSAP(
 			() => {
-				const step = scope.current?.querySelector('[data-step]')!;
+				const step = scope.current!.querySelector('[data-step]');
 				const timeline = gsap.timeline({
 					scrollTrigger: {
 						trigger: scope.current,
