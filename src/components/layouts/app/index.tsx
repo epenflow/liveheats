69da6213bd0fdc02f9e83edf39dde5fd6d7ec215
epenflow <epenflow@gmail.com>
2024-12-31 18:45:00 +0800

    feat(layout/app): Create resources object and integrate dynamic Navbar component into Layout

--- src/components/layouts/app/index.tsx
+++ src/components/layouts/app/index.tsx
@@ -1,12 +1,19 @@
+import dynamic from "next/dynamic";
 import React from "react";
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+	const { Navbar } = resources;
 	const Children = () => children;
 
 	return (
 		<React.Fragment>
+			<Navbar />
 			<Children />
 		</React.Fragment>
 	);
+};
+
+const resources = {
+	Navbar: dynamic(() => import("@/components/base/navbar")),
 };
 export default Layout;
