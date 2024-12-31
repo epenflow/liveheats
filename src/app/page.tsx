efb1c0b4d23efa4f464369035e0ba0f480a5b36d
epenflow <epenflow@gmail.com>
2024-12-31 20:12:09 +0800

    feat(page/home): Update page to dynamically render Views

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
 
 export default function Home() {
-	const { Layout } = resources;
+	const { Layout, Views } = resources;
 	return (
 		<Layout>
 			<main>
-				<h1>Hello World</h1>
+				{Object.entries(Views).map(([key, Component]) => (
+					<Component key={key} />
+				))}
 			</main>
 		</Layout>
 	);
 }
 
 const resources = {
-	Layout: dynamic(() => import("@/components/layouts/app")),
+	Layout: dynamic(() => import('@/components/layouts/app')),
+	Views: {
+		Hero: dynamic(() => import('@/components/views/hero')),
+	},
 };
