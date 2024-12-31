aff5c5c3956851ec665d394cd81b92692b77bc73
epenflow <epenflow@gmail.com>
2024-12-31 20:04:02 +0800

    feat(base/navbar): Enhance Navbar component with dynamic Liveheats and navigation links

--- src/components/base/navbar/index.tsx
+++ src/components/base/navbar/index.tsx
@@ -1,10 +1,32 @@
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import './base.css';
 const Navbar = () => {
-	return <header>navbar</header>;
+	const { Liveheats, navigation } = resources;
+
+	return (
+		<header className="header-container">
+			<nav className="header-navigation">
+				<Liveheats className="liveheats" />
+				<section className="navigation">
+					{navigation.map(({ href, label }) => (
+						<Link
+							key={label}
+							href={href}
+							className="navigation-button">
+							{label}
+						</Link>
+					))}
+				</section>
+			</nav>
+		</header>
+	);
 };
 const resources = {
 	navigation: [
-		{ href: "#", label: "Login" },
-		{ href: "#", label: "Register" },
+		{ href: '#', label: 'Login' },
+		{ href: '#', label: 'Register' },
 	] as Array<{ href: string; label: string }>,
+	Liveheats: dynamic(() => import('@/components/base/liveheats')),
 };
 export default Navbar;
