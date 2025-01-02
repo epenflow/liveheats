5a25dceab862d74dbcf0b13d76a21354e3e33763
epenflow <epenflow@gmail.com>
2025-01-02 19:06:12 +0800

    chore(tailwind): Update configuration for consistent quotation style and add grey color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
 	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
 		extend: {
 			fontFamily: {
 				inter: `var(--font-inter)`,
-				"vt-323": "var(--font-vt323)",
-				"mr-dafoe": "var(--font-mr-dafoe)",
-				lora: "var(--font-lora)",
+				'vt-323': 'var(--font-vt323)',
+				'mr-dafoe': 'var(--font-mr-dafoe)',
+				lora: 'var(--font-lora)',
+			},
+			colors: {
+				grey: {
+					100: 'var(--grey-100)',
+					200: 'var(--grey-200)',
+				},
 			},
 		},
 	},
