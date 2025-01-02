ab0cd0622618928e3e713f97ce8a85d18d060ec5
epenflow <epenflow@gmail.com>
2025-01-02 15:40:46 +0800

    feat(layout): Integrate Lenis for smooth scrolling in the app layout

--- src/components/layouts/app/index.tsx
+++ src/components/layouts/app/index.tsx
@@ -1,19 +1,22 @@
-import dynamic from "next/dynamic";
-import React from "react";
+import dynamic from 'next/dynamic';
+import React from 'react';
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
-	const { Navbar } = resources;
+	const { Navbar, Lenis } = resources;
 	const Children = () => children;
 
 	return (
 		<React.Fragment>
-			<Navbar />
-			<Children />
+			<Lenis>
+				<Navbar />
+				<Children />
+			</Lenis>
 		</React.Fragment>
 	);
 };
 
 const resources = {
-	Navbar: dynamic(() => import("@/components/base/navbar")),
+	Navbar: dynamic(() => import('@/components/base/navbar')),
+	Lenis: dynamic(() => import('@/context/lenis/index')),
 };
 export default Layout;
---
+++ src/context/lenis/index.tsx
@@ -0,0 +1,26 @@
+'use client';
+import gsap from '@/config/gsap';
+import { LenisRef, ReactLenis } from 'lenis/react';
+import React from 'react';
+
+export default function ({ children }: React.PropsWithChildren) {
+	const lenis = React.useRef<LenisRef | null>(null);
+
+	React.useEffect(() => {
+		function update(time: number) {
+			lenis.current?.lenis?.raf(time * 1000);
+		}
+		gsap.ticker.add(update);
+
+		return () => gsap.ticker.remove(update);
+	}, []);
+
+	return (
+		<ReactLenis
+			root
+			ref={lenis}
+			options={{ autoRaf: false }}>
+			{children}
+		</ReactLenis>
+	);
+}
