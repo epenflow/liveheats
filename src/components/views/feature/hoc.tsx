fd08de0e8947ac1841653ec56ac26c7439a32ed4
epenflow <epenflow@gmail.com>
2025-01-06 20:45:02 +0800

    feat(feature): enhance Features component with improved layout and GSAP animations

--- src/components/views/feature/hoc.tsx
+++ src/components/views/feature/hoc.tsx
@@ -1,5 +1,5 @@
 'use client';
-import gsap, { useGSAP } from '@/config/gsap';
+import gsap, { ScrollTrigger, useGSAP } from '@/config/gsap';
 import React from 'react';
 
 export interface Props {
@@ -11,27 +11,57 @@ export default function <T extends object>(Component: React.ComponentType<T & Pr
 
 		useGSAP(
 			() => {
-				const stepper: HTMLElement[] = gsap.utils.toArray('[data-stepper]');
-				const span: HTMLElement[] = gsap.utils.toArray('span');
-				gsap.to(stepper, {
+				const overview: HTMLElement[] = gsap.utils.toArray('.features-overview--inner');
+
+				const overviewTween = gsap.to(overview, {
 					clipPath: (index: number) =>
 						index % 2 === 0 ? 'inset(100% 0% 0% 0%)' : 'none',
-					scrollTrigger: {
-						trigger: scope.current,
-						start: 'top top',
-						end: 'bottom+=100%',
-						scrub: 1.5,
-						pin: true,
-						markers: true,
-					},
+					ease: 'sine.inOut',
 					stagger: {
 						each: 1,
 						amount: 1,
+						// from: 'edges',
 					},
+				});
+
+				ScrollTrigger.create({
+					trigger: scope.current,
+					endTrigger: '[data-stopper="0"]',
+					start: 'top top',
+					end: 'bottom',
+					scrub: 1.5,
+					pin: '.features-overview',
+					animation: overviewTween,
+					pinSpacing: false,
+					// markers: true,
 				});
 			},
 			{ scope },
 		);
+
+		// useGSAP(
+		// 	() => {
+		// 		const stepper: HTMLElement[] = gsap.utils.toArray('[data-stepper]');
+		// 		const span: HTMLElement[] = gsap.utils.toArray('span');
+		// 		gsap.to(stepper, {
+		// 			clipPath: (index: number) =>
+		// 				index % 2 === 0 ? 'inset(100% 0% 0% 0%)' : 'none',
+		// 			scrollTrigger: {
+		// 				trigger: scope.current,
+		// 				start: 'top top',
+		// 				end: 'bottom+=100%',
+		// 				scrub: 1.5,
+		// 				pin: true,
+		// 				markers: true,
+		// 			},
+		// 			stagger: {
+		// 				each: 1,
+		// 				amount: 1,
+		// 			},
+		// 		});
+		// 	},
+		// 	{ scope },
+		// );
 		return <Component {...{ ...props, scope }} />;
 	};
 }
--- src/components/views/feature/index.tsx
+++ src/components/views/feature/index.tsx
@@ -16,80 +16,95 @@ const Features: React.FC<Props> = ({ scope }) => {
 		<section
 			ref={scope}
 			className={cn('features-container', flags['border'] && 'border border-blue-500')}>
-			{features.map(({ src, alt, descriptions, heading }, key) => (
-				<div
-					data-stepper={features.length - key - 1}
-					key={key}
-					style={{
-						zIndex: features.length - key,
-					}}
-					className="features">
-					<div className="features-content">
+			<div className="features-detail">
+				{features.detail.map(({ heading, descriptions }, key) => (
+					<div
+						key={key}
+						data-stopper={key}
+						className="features-content">
 						<div className="features-heading">{heading}</div>
-						<div className="features-descriptions">{descriptions}</div>
+						<div className="features-description">{descriptions}</div>
 					</div>
-					<Image
-						src={src}
-						alt={alt}
-						width={0}
-						height={0}
-						sizes="100vh"
-						className="features-image"
-					/>
-				</div>
-			))}
+				))}
+			</div>
+			<div className="features-overview">
+				{features.overview.map(({ alt, src }, key) => (
+					<div
+						data-overview
+						className="features-overview--inner"
+						key={key}
+						style={{
+							zIndex: features.overview.length - key,
+						}}>
+						<Image
+							src={src}
+							alt={alt}
+							width={0}
+							height={0}
+							sizes="100vh"
+							className="features-image"
+						/>
+					</div>
+				))}
+			</div>
 		</section>
 	);
 };
 const resources = {
-	features: [
-		{
-			src: './Ranking.svg',
-			alt: 'Ranking',
-			heading: (
-				<>
-					<SplitText>Automated rankings -</SplitText>
-					<br />
-					<SplitText>say goodbye to spreadsheets</SplitText>
-				</>
-			),
-			descriptions: (
-				<>
-					<SplitText>Event results automatically drive your season</SplitText>
-					<br />
-					<SplitText>or tour rankings or ratings - with complete</SplitText>
-					<br />
-					<SplitText>flexibility to use your club or tour points system.</SplitText>
-				</>
-			),
-		},
-		{
-			src: './Membership.svg',
-			alt: 'Membership',
-			heading: (
-				<>
-					<SplitText>Registration & membership</SplitText>
-					<br />
-					<SplitText>that your athletes will love</SplitText>
-				</>
-			),
-			descriptions: (
-				<>
-					<SplitText>Members and event entries flow directly</SplitText>
-					<br />
-					<SplitText>into your seed list. Experience powerful tools for</SplitText>
-					<br />
-					<SplitText>
-						payments, waitlisting, discounts, and ability to sell extras.
-					</SplitText>
-				</>
-			),
-		},
-	] satisfies Array<{
-		src: string;
-		alt: string;
-		heading: React.ReactNode;
-		descriptions: React.ReactNode;
-	}>,
+	features: {
+		overview: [
+			{
+				alt: 'Ranking',
+				src: './Ranking.svg',
+			},
+			{
+				alt: 'Overview',
+				src: './Membership.svg',
+			},
+		],
+		detail: [
+			{
+				heading: (
+					<>
+						<SplitText type="char">Automated rankings -</SplitText>
+						<br />
+						<SplitText type="char">say goodbye to spreadsheets</SplitText>
+					</>
+				),
+				descriptions: (
+					<>
+						<SplitText>Event results automatically drive your season</SplitText>
+						<br />
+						<SplitText>or tour rankings or ratings - with complete</SplitText>
+						<br />
+						<SplitText>flexibility to use your club or tour points system.</SplitText>
+					</>
+				),
+			},
+			{
+				heading: (
+					<>
+						<SplitText>Registration & membership</SplitText>
+						<br />
+						<SplitText>that your athletes will love</SplitText>
+					</>
+				),
+				descriptions: (
+					<>
+						<SplitText>Members and event entries flow directly</SplitText>
+						<br />
+						<SplitText>into your seed list. Experience powerful tools for</SplitText>
+						<br />
+						<SplitText>
+							payments, waitlisting, discounts, and ability to sell extras.
+						</SplitText>
+					</>
+				),
+			},
+		],
+	} satisfies {
+		overview: Array<{ alt: string; src: string }>;
+		detail: Array<{ heading: React.ReactNode; descriptions: React.ReactNode }>;
+	},
 };
 export default hoc(Features);
