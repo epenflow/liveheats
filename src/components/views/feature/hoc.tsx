f770e249c8724deff1c7e8c289fe54ac3c60cc23
epenflow <epenflow@gmail.com>
2025-01-06 20:48:37 +0800

    fix(hoc): disable eslint rules for anonymous default export and display name

--- src/components/views/feature/hoc.tsx
+++ src/components/views/feature/hoc.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 'use client';
 import gsap, { ScrollTrigger, useGSAP } from '@/config/gsap';
 import React from 'react';
@@ -5,6 +6,7 @@ import React from 'react';
 export interface Props {
 	scope: React.RefObject<HTMLElement | null>;
 }
+/* eslint-disable react/display-name */
 export default function <T extends object>(Component: React.ComponentType<T & Props>) {
 	return function (props: T) {
 		const scope = React.useRef<HTMLElement>(null);
